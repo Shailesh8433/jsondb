654c95f17c88a17d1f198743a62117a6691b6098
Shailesh8433 <80758715+Shailesh8433@users.noreply.github.com>
2021-03-29 00:12:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,428 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+      <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+      <link rel="icon" href="img/favicon_io/favicon.ico" type="image/gif" sizes="16x16">
+
+    <title>ESQS Infotech</title>
+  </head>
+  <body onload="myFunction()">
+      <div id="loader">
+          <div></div>
+           <div></div>
+           <div></div>
+      </div>
+   
+    
+      <div id="header" class="sticky-top">		
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <div class="container">
+    <a href="#" class="navbar-brand">ES<i class="fa fa-superpowers"></i>S  <span>INF<i class="fa fa-superpowers"></i>TECH</span></a>	
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mynavbar" >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class=" collapse navbar-collapse" id="mynavbar">
+        <ul class="navbar-nav ">
+          <li class="nav-item active">
+            <a class="nav-link " href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link scroll" href="#services" data-target="">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#About">About US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact us</a>
+          </li>
+        </ul> 
+        
+      </div>
+</div>
+</nav>
+</div>
+
+<div Id="Slider">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" id="slider" >
+    <div class="carousel-item active">
+      <img class="d-block w-100 " src="img/slide1.jpg"  alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+                  <p>Trusted Services Repair your Laptop & Computer
+                      <br>
+                  "We work with diffrrent client & provide solution to client with their Hardware and Software problem."</p>
+                </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/slide2.jpg" alt="Second slide">
+        
+        <div class="carousel-caption d-none d-md-block align-content-start">
+                  <p>Query Hardware and Software
+                    <br>
+                  "Prepare For New Future Our work is presentation of our capabilities."</p>
+    </div>
+      </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/slide3.jpg" alt="Third slide">
+          <div class="carousel-caption d-none d-md-block">
+                  <p>Repair Your Laptop
+                      <br>
+                  "Our exprience and service is our strength."</p>
+                </div>
+    </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+      </div>
+
+
+<div id="services" >
+  <div class="container py-5 ">
+    <div class="row">
+        <div class="col-md-12 service-tittle">
+              <h1> OUR SERVICES</h1>
+              <strong><p>We provide a wide range of creative services</p> </strong>
+        </div>
+      </div>
+    </div>
+      <div class="container-fluid">
+          <div class="row">
+          <div class="col-lg-4 col-md-4" data-aos="fade-right" id="icons" >
+                <i class="fa fa-desktop"></i>
+                <h4> Desktop Repairing</h4>
+                <p>
+                      Repairing and Installing Hardware (Motherboard, CD-DVD ROM, Power Supply, etc.) and Software (Operating System, Driver, Antivirus, etc.) on Desktop computer.
+                    </p>
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 "id="icons">
+                <i class="fa fa-laptop"></i>
+         
+                <h4> Laptop Repairing</h4>
+                <p>Repairing and Installing Hardware (Battery, RAM, Keyboard, Screen service, Networking, etc.) and Software (Operating System, Driver, Antivirus, etc.) on Laptop Computer.</p>
+                
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+            </div>
+              
+              
+            <div class="col-lg-4 col-md-4"data-aos="fade-left" id="icons">
+                <i class="fa fa-signal"></i>
+      
+                <h4> Networking</h4>
+                <p>Creating and  Maintaining computer networks and related computing environments including systems software, applications software, hardware, and configurations.</p>
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+            </div>
+          </div>
+    </div>
+              
+     <div class="container">
+          <div class="row">
+          <div class="col-lg-4  col-md-4 "data-aos="fade-right"id="icons">
+                <i class="fa fa-download"></i>
+       
+                <h4> Data Recovery</h4>
+                <p>We do data recovery for lost  data from Damaged, Virus Infected, Corrupted, Failed or Inaccessible torage from your Laptop Hard Disk, Portable Hard Drive, Pendrive etc.</p>
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+            </div>
+        
+        <div class="col-lg-4 col-md-4" id="icons">
+            <i class="fa fa-recycle"></i>
+       
+                <h4> Computer Assembling</h4>
+                <p>We do prepare computers by assembling hardware and configuring software as per client requirements or suggested hardware for client personal and business uses.</p>
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+            </div>
+        <div class="col-lg-4 col-md-4 " data-aos="fade-left" id="icons">
+            <i class="fa fa-area-chart"></i>
+       
+                <h4> Annual Maintenance</h4>
+                <p>We provide regular maintenance for client personal and business computers and other perishable under Computer AMC Services.</p>
+            
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+        </div>
+         </div>
+          </div>
+    
+    <div class="container-fluid">
+          <div class="row">
+          <div class=" col-lg-4  col-md-4 " data-aos="fade-right" id="icons">
+                    <i class="fa fa-desktop"></i>
+       
+                <h4>Sales & Service</h4>
+                <p>
+                      Repairing and Installing Hardware (Motherboard, CD-DVD ROM, Power Supply, etc.) and Software (Operating System, Driver, Antivirus, etc.) on Desktop computer.
+                    </p>
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+
+            </div>
+                <div class="col-lg-4 col-md-4" id="icons">
+                    <i class="fa fa-briefcase"></i>
+       
+                    <h4> Digital Solution</h4>
+                <p>We provide digital solutions. Looking for a software or Digital solution for a business like( Web Site development, Mobile appdevelopment, Software Developoment etc) </p>
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+                 </div>
+                <div class="col-lg-4 col-md-4"data-aos="fade-left"id="icons">
+                        <i class="fa fa-cubes"></i>
+                <h4> Digital Solution</h4>
+                <p>We provide digital solutions. Looking for a software or Digital solution for a business like( Web Site development, Mobile app development, Software Developoment etc) </p>
+                <div class="readmore">
+                  <a href="#">Read more</a>
+                </div>
+            </div>    
+         </div>
+          </div>
+    
+</div>
+      <style>
+      .brand a:hover{
+          color: brown;}
+          
+          </style>
+      
+      <div id="About">
+      <div class="container py-5" >
+          <div class="col-md-12 cont">
+          <h1 class="text-center">About Us
+              </h1><strong></strong>
+          
+          </div>
+          </div>
+          
+          <div class="container">
+          <div class="row">
+              <div class="col-md-7 bg-light" data-aos="fade-right">
+              <h1 class="pb-2">welcome to ES<i class="fa fa-superpowers text-danger"></i>S  <span>INF<i class="fa fa-superpowers text-danger"></i>TECH</span>, A Complete Hardware And Software Solutions</h1>
+            <p> ESOS INFOTECH devoted itself to the existing fields of information Technology for quite some time now almost we have completed 21 years in this industry. The company started its business in the competitive area of Desktop Pc starting from 286 processor computers. Now the company has explored the world of Information Technology and is striding forward with renewal zeal and innovative concepts.</p>
+                  <p>
+            ESOS INFOTECH provides solutions that are feature rich, robust and scalable. It is our constant endeavour to provide customers with more value for money. ESOS INFOTECH have professional engineers who do onsite servicing for all devices. Being a service-oriented industry for a long time ESOS INFOTECH have master the art of maintaining office automation products. We offer comprehensive and non-comprehensive yearly contracts. ESOS INFOTECH is a stockist and wholesaler for mother boards, screen LCD-led, Adapters, Touch panel.
+                  </p>
+              
+              </div>
+              
+              <div class="col-md-5 md" data-aos="fade-left">
+              <img src="img/electronics-2929043.jpg">
+              </div>
+              <div class="col-12 pt-4  text-center"data-aos="fade-up">
+              <p>ESOS INFOTECH has seen the evolution in the changing technology from the last two decade. We have adopted and learned This new technology which helped us to master any device and Now Data Recovery. ESOS INFOTECH must have repaired and service more than Fifty thousand of all electronic devices used in the field of IT. At last, we would like to add that more than Lac satisfied users find a way into ESOS INFOTECH for various services.</p>
+              </div>
+              </div>
+          </div>
+      </div>
+    
+    <div id="contact" class=" py-5"> 
+        <div class="container">
+        <div class="row" >
+           <div class="col-md-12"data-aos="fade-up">
+         <h1 id="contact1" class="text-center"> CONTACT US</h1>
+        </div>
+            </div>
+        </div>
+      </div>
+    <div id="banner">
+            <div class="container bg-light ">
+                <div class="row">
+                <div class="col-lg-6 col-md-12" id="quick">
+                    <h4 class="text-black"> Quick Contact</h4>
+                    <ul>
+                        <li class="">
+                        <i class="fa fa-map-marker"> </i>
+                    <b>Location:</b>
+                <span>201, Jai Ganesh Rahi Washi Sewa Mandal, Albert Comp,Sankar Nagar, Vasri Hill, Goregaon West Mumbai 400104</span>
+                        </li>
+                    
+                        <li>
+                        <i class="fa fa-phone"> </i>
+                    <b>Contact:</b>
+                <span>Mobile : +91 8080308012</span>
+                        </li>
+                    
+                        <li>
+                        <i class="fa fa-envelope"> </i>
+                    <b>Email Id:</b>
+                <span>esqsinfotech@gmail.com</span>
+                        </li>
+                    
+                    
+                    </ul>
+                   
+                    </div>  
+          
+    
+          <div class="col-md-12 col-lg-6 bg-light m" data-aos="fade-right">
+
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Enter First name</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter First name">
+  </div>
+                    <div class="form-group">
+    <label for="exampleFormControlInput1">Enter Last name</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter last name">
+  </div>
+              <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Subject</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+<button class="btn btn-lg btn-primary" type="button"> save</button>
+    </div>
+                </div>
+        </div>
+        </div>
+      
+      
+      
+      
+        <div id="map" class="mt-4">
+              <div class="container-fluid">      
+                <div class="row ">
+            
+                
+                  <div class="col-md-12 responsive" data-aos="fade-up">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3772.131061746728!2d72.83027301490016!3d19.013945587123576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cec197909a0d%3A0x1a918fbfd4433ad0!2sDharwala.com!5e0!3m2!1sen!2sin!4v1616910596182!5m2!1sen!2sin" width=100% height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                 </div>
+                </div>
+        </div>
+    </div>
+
+<div id="footer-side">
+    <div class="container">
+        <div class="row">
+        
+        <div class="col-lg-5 col-sm-6">
+            <div class="brand">
+        <a href="index.php" class="navbar-brand text-red">ES<i class="fa fa-superpowers"></i>S  <span>INF<i class="fa fa-superpowers"></i>TECH</span></a>	
+                </div>
+        <div class="social">
+         <a href="#" ><i class="fa fa-instagram"></i></a>
+        
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-whatsapp"></i></a>
+            <a href="#"><i class="fa fa-telegram"></i></a>
+            <a href="#"><i class="fa fa-linkedin"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+        </div>
+    </div>
+            
+                <div class="col-lg-3 col-sm-4 " id="nav">
+                  <li><a  href="index.php">Home</a></li>
+                <li ><a  href="#services" >Services</a></li>
+                    <li><a href="#About">About US</a></li>
+              <li ><a href="#contact">Contact us</a></li>
+
+            </div>
+            
+            <div class="col-lg-4 col-sm-12" id="add">
+            
+            <li class="text-white"> Mobile : +91 8080308012</li>
+                <li class="text-white">Email : <a href="mailto:esqsinfotech@gmail.com">esqsinfotech@gmail.com</a></li>
+                <li class="text-white">
+                  <Address>Address : 201, Jai Ganesh Rahi Washi Sewa Mandal, Albert Comp,<br>Sankar Nagar, Vasri Hill,<br> Goregaon West Mumbai 400104</Address>
+            </div>
+
+                </div>
+        <h5 class="text-center text-white pb-4"> Note: More then 10 years we are doing computer Hardware and Software sol</h5>
+            </div>
+
+    
+    </div>
+    <div id="footer">
+    <div class="container py-2">
+        <div class="row justify-content-center">
+        <div class="col-sm-12 text-center">
+            
+           <h3> @copyright2021 <prev><a href="#">ES<i class="fa fa-superpowers"></i>S  <span>INF<i class="fa fa-superpowers"></i>TECH</span></a></prev> </h3> 
+            
+            </div>
+        
+            <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button>
+            
+        
+        </div>
+        
+        </div>
+    
+    </div>
+    
+ <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+      <script>
+       
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
+      <script>
+          var preloader= document.getElementById('loader');
+          function myFunction(){
+              
+              preloader.style.display='none';
+          }
+      </script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
